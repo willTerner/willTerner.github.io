7bb191216238c7e2d378caaef188273b9f300ad7
willTerner <75409032+willTerner@users.noreply.github.com>
2022-02-25 11:09:18 +0800

    deploy

---
+++ assets/main.7722bff2.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";let e=document.querySelector("#show-image");e.onclick=function(){document.querySelector("#picture-holder").classList.add("picture-holder")};
---
+++ assets/nested.8a396bb9.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";let e=document.querySelector("form");e.onsubmit=function(r){r.preventDefault(),alert(e.username.value+e.password.value)};
